20b79cf1c4ea63d1a3e7cbbbb3cc623dd4c2bdc8
KeyurM12 <keyurmhaisekar@gmail.com>
2021-05-11 22:48:04 +0530

    commit 5

--- app/src/main/java/com/example/mykotlin1/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin1/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.mykotlin1
 
 import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txt1 = findViewById<TextView>(R.id.txt1)
+        var txt2 = findViewById<TextView>(R.id.txt2)
+        var btn = findViewById<Button>(R.id.button2)
+        var input_radius = findViewById<TextView>(R.id.input_radius)
+
+        btn.setOnClickListener(View.OnClickListener{
+            var radius = 0.0
+            val pi = 3.1415
+            radius = input_radius.text.toString().toDouble()
+
+            var area = pi*radius*radius
+
+            txt1.text = "Area of the Circle is $area"
+
+
+        })
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/mykotlin1/Program_1.kt
@@ -0,0 +1,11 @@
+package com.example.mykotlin1
+
+fun main()
+{
+    var name:String = "Android Kotlin"
+    var fNum:Float = 2.8F
+    val pi:Double = 3.141592653589793238
+    var num:Int = 25
+
+    println("$name $fNum $pi $num")
+}
