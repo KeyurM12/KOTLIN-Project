f78226d804869bc970312b4b4baad9186cdbaf1d
KeyurM12 <keyurmhaisekar@gmail.com>
2021-05-12 20:28:24 +0530

    commit 6

--- app/src/main/java/com/example/mykotlin1/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.R.attr.button
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var txt1 = findViewById<TextView>(R.id.txt1)
-        var txt2 = findViewById<TextView>(R.id.txt2)
         var btn = findViewById<Button>(R.id.button2)
-        var input_radius = findViewById<TextView>(R.id.input_radius)
+        var input_radius = findViewById<EditText>(R.id.input_radius)
 
         btn.setOnClickListener(View.OnClickListener{
-            var radius = 0.0
             val pi = 3.1415
-            radius = input_radius.text.toString().toDouble()
+            var radius = input_radius.text.toString().toDouble()
 
             var area = pi*radius*radius
 
             txt1.text = "Area of the Circle is $area"
-
-
         })
 
 
