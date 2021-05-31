27c1447a7209e87b4c7a49b8b0d50d1d0a63c0a8
KeyurM12 <keyurmhaisekar@gmail.com>
2021-05-31 21:23:11 +0530

    commit 8

---
+++ app/src/main/java/com/example/mykotlin1/MainActivity3.kt
@@ -0,0 +1,34 @@
+package com.example.mykotlin1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        var txt1 = findViewById<TextView>(R.id.txt)
+        var btn = findViewById<Button>(R.id.btn)
+        var input_num = findViewById<EditText>(R.id.NumInput)
+        btn.setOnClickListener(View.OnClickListener {
+            var i = 1
+            var str:String =""
+            var num = input_num.text.toString().toInt()
+            while(i!=num)
+            {
+                str+="$i@"
+                i++
+            }
+            str+="$i"
+            txt1.text=str
+        })
+
+
+
+    }
+}
--- app/src/main/java/com/example/mykotlin1/Program_1.kt
+++ app/src/main/java/com/example/mykotlin1/Program_1.kt
@@ -2,10 +2,12 @@ package com.example.mykotlin1
 
 fun main()
 {
-    var name:String = "Android Kotlin"
-    var fNum:Float = 2.8F
-    val pi:Double = 3.141592653589793238
-    var num:Int = 25
-
-    println("$name $fNum $pi $num")
+    var x=100;
+    var sum=0;
+    while(x!=0)
+    {
+        sum+=x
+        x--
+    }
+    println(sum)
 }
